412ee6acd20d98a3b5fb276e1f299f6d9d174f1c
SHOBHIT-RV-SHARMA <shobhitsharmashma@gmail.com>
2024-10-27 16:30:59 +0530

    New Commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate")
+const wrapAsync = require("./utilities/wrapAsync.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/StayNest";
 
@@ -51,10 +52,16 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
+app.post("/listings", wrapAsync , async (req, res ,next) => {
+  try{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  }
+  catch(err){
+    next(err);
+  }
+ 
 });
 
 //Edit Route
@@ -92,6 +99,10 @@ app.delete("/listings/:id", async (req, res) => {
 //   console.log("sample was saved");
 //   res.send("successful testing");
 // });
+
+app.use((err,req,res,next) => {
+  res.send("something went wrong :|");
+})
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
